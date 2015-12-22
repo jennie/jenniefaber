ad60f44013a285de198b491a61ca63ccb41591ca
Jennie Faber <jennie@bentobox.net>
2015-12-22 09:53:48 -0500

    Image updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox({'always_show_close': false});
+});
